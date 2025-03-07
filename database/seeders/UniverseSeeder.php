b51041a63ce4a4274f12ebfcc65ee1099e391536
aurora11gar <villaaurora375@gmail.com>
2025-03-06 23:24:55 -0600

    Controllers

---
+++ database/seeders/UniverseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UniverseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Universe::create([
+            ['name' => 'Marvel', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['name' => 'DC', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+        ]);
+        Universe::create([
+            ['name' => 'DC', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+        ]);
+
+    }
+}
