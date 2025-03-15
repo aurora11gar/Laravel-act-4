7a9ac9ad8c302a88bff74ef6d57ab9da4c4d54fb
aurora11gar <villaaurora375@gmail.com>
2025-03-15 00:20:01 -0600

    blade

--- app/Http/Controllers/GenderController.php
+++ app/Http/Controllers/GenderController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use App\Models\Gender;
 
@@ -10,7 +9,8 @@ class GenderController extends Controller
     public function index(){
 
         $gender = Gender::all();
-    
-        return view('gender', compact('gender'));
+
+        return view('gender',compact('gender'));
+
     }
 }
